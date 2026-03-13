407f0ed841ceab765fcf8fe864908a5105dd51ea
Rachel <94303284+rgwhite210@users.noreply.github.com>
2026-03-12 18:26:14 -0700

    add QuizGrader class

--- src/QuizGrader.java
+++ src/QuizGrader.java
@@ -1,1 +1,18 @@
+public class QuizGrader {
 
+    public static void printSpecs() {
+        System.out.println("==============================");
+        System.out.println("   Quiz Grade Calculator");
+        System.out.println("==============================");
+        System.out.println("This program allows a student to:");
+        System.out.println("  - Enter up to 10 quiz scores");
+        System.out.println("  - Calculates the average score");
+        System.out.println("  - Displays the letter grade");
+        System.out.println("  - Enter 999 at any time to quit");
+        System.out.println("==============================");
+    }
+
+    public static void main(String[] args) {
+        printSpecs();
+    }
+}
